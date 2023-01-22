797b6c12be2e726259e5d5d021f9398d08912264
Sebastian Muszynski <s.muszynski@constructiva.de>
2023-01-22 20:30:22 +0100

    Introduce sensor entities

--- components/votronic_ble/sensor.py
+++ components/votronic_ble/sensor.py
@@ -5,13 +5,18 @@ from esphome.const import (
     CONF_CURRENT,
     CONF_POWER,
     DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_EMPTY,
+    DEVICE_CLASS_ENERGY,
     DEVICE_CLASS_POWER,
     DEVICE_CLASS_VOLTAGE,
     ICON_EMPTY,
     STATE_CLASS_MEASUREMENT,
     UNIT_AMPERE,
+    UNIT_EMPTY,
+    UNIT_PERCENT,
     UNIT_VOLT,
     UNIT_WATT,
+    UNIT_WATT_HOURS,
 )
 
 from . import CONF_VOTRONIC_BLE_ID, VotronicBle
@@ -22,39 +27,142 @@ CODEOWNERS = ["@syssi"]
 
 CONF_TOTAL_VOLTAGE = "total_voltage"
 
+CONF_BATTERY_VOLTAGE = "battery_voltage"
+CONF_SECONDARY_BATTERY_VOLTAGE = "secondary_battery_voltage"
+CONF_BATTERY_CAPACITY = "battery_capacity"
+CONF_STATE_OF_CHARGE = "state_of_charge"
+# CONF_CURRENT = ""
+CONF_BATTERY_NOMINAL_CAPACITY = "battery_nominal_capacity"
+CONF_PV_VOLTAGE = "pv_voltage"
+CONF_PV_CURRENT = "pv_current"
+CONF_BATTERY_STATUS_BITMASK = "battery_status_bitmask"
+CONF_CONTROLLER_STATUS_BITMASK = "controller_status_bitmask"
+CONF_CHARGED_CAPACITY = "charged_capacity"
+CONF_CHARGED_ENERGY = "charged_energy"
+CONF_PV_POWER = "pv_power"
+
 ICON_CURRENT_DC = "mdi:current-dc"
 
-UNIT_SECONDS = "s"
-UNIT_HOURS = "h"
 UNIT_AMPERE_HOURS = "Ah"
 
+ICON_BATTERY_CAPACITY = "mdi:battery-medium"
+ICON_STATE_OF_CHARGE = "mdi:battery-50"
+ICON_BATTERY_NOMINAL_CAPACITY = "mdi:battery-100"
+
+ICON_BATTERY_STATUS_BITMASK = "mdi:alert-circle-outline"
+ICON_CONTROLLER_STATUS_BITMASK = "mdi:alert-circle-outline"
+ICON_CHARGED_CAPACITY = "mdi:battery-charging"
+
+ICON_CHARGING_CYCLES = "mdi:battery-sync"
+ICON_ERRORS_BITMASK = "mdi:alert-circle-outline"
+
 SENSORS = [
-    CONF_TOTAL_VOLTAGE,
+    CONF_BATTERY_VOLTAGE,
+    CONF_SECONDARY_BATTERY_VOLTAGE,
+    CONF_BATTERY_CAPACITY,
+    CONF_STATE_OF_CHARGE,
     CONF_CURRENT,
-    CONF_POWER,
+    CONF_BATTERY_NOMINAL_CAPACITY,
+    CONF_PV_VOLTAGE,
+    CONF_PV_CURRENT,
+    CONF_BATTERY_STATUS_BITMASK,
+    CONF_CONTROLLER_STATUS_BITMASK,
+    CONF_CHARGED_CAPACITY,
+    CONF_CHARGED_ENERGY,
+    CONF_PV_POWER,
 ]
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_VOTRONIC_BLE_ID): cv.use_id(VotronicBle),
-        cv.Optional(CONF_TOTAL_VOLTAGE): sensor.sensor_schema(
+        cv.Optional(CONF_BATTERY_VOLTAGE): sensor.sensor_schema(
             unit_of_measurement=UNIT_VOLT,
             icon=ICON_EMPTY,
             accuracy_decimals=2,
             device_class=DEVICE_CLASS_VOLTAGE,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
+        cv.Optional(CONF_SECONDARY_BATTERY_VOLTAGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_VOLT,
+            icon=ICON_EMPTY,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_VOLTAGE,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_BATTERY_CAPACITY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE_HOURS,
+            icon=ICON_BATTERY_CAPACITY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_STATE_OF_CHARGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_PERCENT,
+            icon=ICON_STATE_OF_CHARGE,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
         cv.Optional(CONF_CURRENT): sensor.sensor_schema(
             unit_of_measurement=UNIT_AMPERE,
             icon=ICON_CURRENT_DC,
-            accuracy_decimals=1,
+            accuracy_decimals=2,
             device_class=DEVICE_CLASS_CURRENT,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_BATTERY_NOMINAL_CAPACITY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE_HOURS,
+            icon=ICON_BATTERY_NOMINAL_CAPACITY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_PV_VOLTAGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_VOLT,
+            icon=ICON_EMPTY,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_VOLTAGE,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_PV_CURRENT): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE,
+            icon=ICON_CURRENT_DC,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_CURRENT,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_BATTERY_STATUS_BITMASK): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_BATTERY_STATUS_BITMASK,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_CONTROLLER_STATUS_BITMASK): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_CONTROLLER_STATUS_BITMASK,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_CHARGED_CAPACITY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE_HOURS,
+            icon=ICON_CHARGED_CAPACITY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_EMPTY,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_CHARGED_ENERGY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_WATT_HOURS,
+            icon=ICON_CHARGED_CAPACITY,
+            accuracy_decimals=0,
+            device_class=DEVICE_CLASS_ENERGY,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_POWER): sensor.sensor_schema(
             unit_of_measurement=UNIT_WATT,
             icon=ICON_EMPTY,
-            accuracy_decimals=1,
+            accuracy_decimals=0,
             device_class=DEVICE_CLASS_POWER,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
