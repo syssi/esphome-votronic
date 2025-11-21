fb4acd4f2353351e2923027014302748194a7d33
Sebastian Muszynski <basti@linkt.de>
2025-11-21 16:38:28 +0100

    Revert

--- components/votronic/text_sensor.py
+++ components/votronic/text_sensor.py
@@ -74,21 +74,6 @@ CONFIG_SCHEMA = VOTRONIC_COMPONENT_SCHEMA.extend(
         cv.Optional(CONF_PV_CONTROLLER_STATUS): text_sensor.text_sensor_schema(
             text_sensor.TextSensor, icon="mdi:heart-pulse"
         ),
-        cv.Optional(
-            CONF_CHARGING_CONVERTER_MODE_SETTING
-        ): text_sensor.text_sensor_schema(
-            text_sensor.TextSensor, icon="mdi:car-battery"
-        ),
-        cv.Optional(
-            CONF_CHARGING_CONVERTER_BATTERY_STATUS
-        ): text_sensor.text_sensor_schema(
-            text_sensor.TextSensor, icon="mdi:car-battery"
-        ),
-        cv.Optional(
-            CONF_CHARGING_CONVERTER_CONTROLLER_STATUS
-        ): text_sensor.text_sensor_schema(
-            text_sensor.TextSensor, icon="mdi:heart-pulse"
-        ),
     }
 )
 
