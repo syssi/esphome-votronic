952c374ae0be08361f0b97ed37432c84dd418df8
Sebastian Muszynski <basti@linkt.de>
2026-03-24 08:20:27 +0100

    Fix deprecated ESPBTUUID::to_string() → to_str() (#81)

--- components/votronic_ble/votronic_ble.cpp
+++ components/votronic_ble/votronic_ble.cpp
@@ -1,13 +1,6 @@
 #include "votronic_ble.h"
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
-#include "esphome/core/version.h"
-
-#if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 12, 0)
-#define ADDR_STR(x) x
-#else
-#define ADDR_STR(x) (x).c_str()
-#endif
 
 namespace esphome {
 namespace votronic_ble {
@@ -69,7 +62,7 @@ void VotronicBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t 
           this->parent_->get_characteristic(this->service_monitoring_uuid_, this->char_battery_computer_uuid_);
       if (char_battery_computer == nullptr) {
         ESP_LOGW(TAG, "[%s] No battery computer characteristic found at device. No battery computer attached?",
-                 ADDR_STR(this->parent_->address_str()));
+                 this->parent_->address_str());
         break;
       }
       this->char_battery_computer_handle_ = char_battery_computer->handle;
@@ -84,7 +77,7 @@ void VotronicBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t 
           this->parent_->get_characteristic(this->service_monitoring_uuid_, this->char_solar_charger_uuid_);
       if (char_solar_charger == nullptr) {
         ESP_LOGW(TAG, "[%s] No solar charger characteristic found at device. No solar charger attached?",
-                 ADDR_STR(this->parent_->address_str()));
+                 this->parent_->address_str());
         break;
       }
       this->char_solar_charger_handle_ = char_solar_charger->handle;
@@ -117,7 +110,7 @@ void VotronicBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t 
 
 void VotronicBle::update() {
   if (this->node_state != espbt::ClientState::ESTABLISHED) {
-    ESP_LOGW(TAG, "[%s] Not connected", ADDR_STR(this->parent_->address_str()));
+    ESP_LOGW(TAG, "[%s] Not connected", this->parent_->address_str());
     return;
   }
 }
@@ -229,11 +222,16 @@ void VotronicBle::decode_solar_charger_data_(const std::vector<uint8_t> &data) {
 
 void VotronicBle::dump_config() {
   ESP_LOGCONFIG(TAG, "VotronicBle:");
-  ESP_LOGCONFIG(TAG, "  MAC address                         : %s", ADDR_STR(this->parent_->address_str()));
-  ESP_LOGCONFIG(TAG, "  Monitoring Service UUID             : %s", this->service_monitoring_uuid_.to_string().c_str());
+  ESP_LOGCONFIG(TAG, "  MAC address                         : %s", this->parent_->address_str());
+  char service_monitoring_uuid_str[esp32_ble::UUID_STR_LEN];
+  char char_battery_computer_uuid_str[esp32_ble::UUID_STR_LEN];
+  char char_solar_charger_uuid_str[esp32_ble::UUID_STR_LEN];
+  ESP_LOGCONFIG(TAG, "  Monitoring Service UUID             : %s",
+                this->service_monitoring_uuid_.to_str(service_monitoring_uuid_str));
   ESP_LOGCONFIG(TAG, "  Battery Computer Characteristic UUID: %s",
-                this->char_battery_computer_uuid_.to_string().c_str());
-  ESP_LOGCONFIG(TAG, "  Solar Charger Characteristic UUID   : %s", this->char_solar_charger_uuid_.to_string().c_str());
+                this->char_battery_computer_uuid_.to_str(char_battery_computer_uuid_str));
+  ESP_LOGCONFIG(TAG, "  Solar Charger Characteristic UUID   : %s",
+                this->char_solar_charger_uuid_.to_str(char_solar_charger_uuid_str));
 
   LOG_BINARY_SENSOR("", "Charging", this->charging_binary_sensor_);
   LOG_BINARY_SENSOR("", "Discharging", this->discharging_binary_sensor_);
